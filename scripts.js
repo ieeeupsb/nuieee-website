44ff6e31a42a532192c38e8d22c6e7ed32f911bc
Zé Bateira <jlageb+github@gmail.com>
2013-11-09 17:47:47 +0000

    Navigation bar: animação onclick adicionada.

---
+++ scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+	$('.nav-link').click(function(event) {
+		$('html:not(:animated),body:not(:animated)').animate({
+			scrollTop: $($(this).attr('href')).offset().top
+		}, 500);
+
+		window.location.hash = $(this).attr('href');
+		event.preventDefault();
+	});
+});
