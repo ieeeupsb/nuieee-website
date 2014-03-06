7b9c5d65cf74c3bbde3317b04be448d84f014383
Zé Bateira <jlageb+git@gmail.com>
2014-03-06 17:34:15 +0000

    1st speaker added. jquery removed for now.

--- conferences/ieeetalks/js/main.js
+++ conferences/ieeetalks/js/main.js
@@ -1,12 +1,8 @@
-$(function() {
-  function initialize() {
-    var mapOptions = {
-      center: new google.maps.LatLng(41.1782642,-8.5956336),
-      zoom: 17,
-      scrollwheel: false
-    };
-    var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
-  }
-
-  google.maps.event.addDomListener(window, 'load', initialize);
-});
+window.onload = function() {
+  var mapOptions = {
+    center: new google.maps.LatLng(41.1782642,-8.5956336),
+    zoom: 17,
+    scrollwheel: false
+  };
+  var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+};
