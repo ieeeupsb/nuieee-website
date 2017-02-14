ac8203ab68c11b12f0fcf851aa531786a2be9eae
Diogo Eiras <eiras@outlook.com>
2017-02-14 18:59:30 +0000

    Improving the deploy method

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -2,7 +2,7 @@
 set -x
 if [ $TRAVIS_BRANCH == 'master' ] ; then
     cd _site
-    lftp -c "open -u $USERNAME,$PASSWORD homes.fe.up.pt; set ssl:verify-certificate no; mirror -R ./ /public_html"
+    lftp -c "open -u $USERNAME,$PASSWORD homes.fe.up.pt; set ssl:verify-certificate no; mirror -R ./ /public_html --ignore-time"
 else
     echo "Not deploying, since this branch isn't master."
 fi
