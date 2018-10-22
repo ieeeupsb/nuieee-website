d44cf242948f0af516748d42146a9d3bba37a373
Daniel Silva <danny@dannyps.net>
2018-10-22 13:10:26 +0100

    Try some debugging

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -2,7 +2,7 @@
 set -x
 if [ $TRAVIS_BRANCH == 'master' ] ; then
     cd _site
-    lftp -c "open -u $USERNAME,$PASSWORD $DOMAIN; set ssl:verify-certificate no; mirror -R ./ / --ignore-time"
+    lftp -vv -c "open -u $USERNAME,$PASSWORD $DOMAIN; set ssl:verify-certificate no; mirror -R ./ / --ignore-time"
 else
     echo "Not deploying, since this branch isn't master."
 fi
