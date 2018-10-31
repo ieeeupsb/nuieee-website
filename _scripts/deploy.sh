2f9fd245fe9ade948fb99774368aa985a9534f35
Daniel Silva <danny@dannyps.net>
2018-10-31 09:06:46 +0000

    Reset deploy script

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -2,7 +2,6 @@
 set -x
 if [ $TRAVIS_BRANCH == 'master' ] ; then
     cd _site
-	telnet $DOMAIN 21
     lftp -c "open -u $USERNAME,$PASSWORD $DOMAIN; set ssl:verify-certificate no; mirror -R ./ / --ignore-time"
 else
     echo "Not deploying, since this branch isn't master."
