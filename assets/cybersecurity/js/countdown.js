b5d8463fa5f7a1ad173553aadfb109bd4cd5723a
Fábio Gaspar <fabiodanielreis@hotmail.com>
2019-03-10 23:57:03 +0000

    Fix to hide countdown when it reaches 0

--- assets/cybersecurity/js/countdown.js
+++ assets/cybersecurity/js/countdown.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Mar 11, 2019 09:00:00").getTime();
+var countDownDate = new Date("Mar 11, 2019 14:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function () {
@@ -10,6 +10,13 @@ var x = setInterval(function () {
     // Find the distance between now and the count down date
     var distance = countDownDate - now;
 
+    // If the count down is over, write some text 
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("timer").style.visibility = "hidden";
+        return;
+    }
+    
     // Time calculations for days, hours, minutes and seconds
     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
@@ -22,10 +29,4 @@ var x = setInterval(function () {
     document.getElementById("minutes").innerHTML = minutes;
     document.getElementById("seconds").innerHTML = seconds;
 
-
-    // If the count down is over, write some text 
-    if (distance < 0) {
-        clearInterval(x);
-        document.getElementById("timer").style.visibility = "none";
-    }
 }, 1000);
